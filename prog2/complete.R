9e2b4c27e26824dc863f9b482110b96a41749e45
Alex Auritt <alexauritt@gmail.com>
2014-01-19 12:33:26 -0500

    complete (part 2) script and test output

--- prog2/complete.R
+++ prog2/complete.R
@@ -1,3 +1,5 @@
+source('getmonitor.R')
+
 complete <- function(directory, id = 1:332) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
@@ -12,4 +14,22 @@ complete <- function(directory, id = 1:332) {
         ## ...
         ## where 'id' is the monitor ID number and 'nobs' is the
         ## number of complete cases
+        stuff <- data.frame(id, 'smelly')
+
+        completeCount = c(length(id))
+        
+        index = 1
+        for (i in id) {
+          curData <- getmonitor(i, directory)
+          completeCount[index] = completeObservationCount(curData)
+          index <- index + 1
+        }
+
+        data.frame(id, 'nobs' = completeCount)
+}
+
+completeObservationCount <- function(data) {
+  sulfateTaken <- subset(data, sulfate != 'NA')
+  completeRows <- subset(sulfateTaken, nitrate != 'NA')
+  nrow(completeRows)
 }
