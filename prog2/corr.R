a38f769cc488fd2a02e8dea0d971b433ac5ce6dd
Alex Auritt <alexauritt@gmail.com>
2014-01-19 17:10:49 -0500

    cleanup

--- prog2/corr.R
+++ prog2/corr.R
@@ -12,40 +12,12 @@ corr <- function(directory, threshold = 0) {
         ## Return a numeric vector of correlations
         
         counts <- complete(directory)
-
         sufficients <- subset(counts, nobs >= threshold)
 
         datacor <- function(monitorNum) {
           completeMonitorData <- getcompletemonitor(monitorNum, directory)
           cor(completeMonitorData$sulfate, completeMonitorData$nitrate)
-          # print("I have complete data for something")
-          # print(class(monitorNum))
-          # print(monitorNum)
-          # print("\n")
         }
 
-        # print('num 1')
-        # print(sufficients[1])        
-        # print(sufficients$id)
         sapply(sufficients$id, datacor)
-
-        # cors = integer()
-        
-        # for (i in sufficients$id)
-          # print(i)
-          # curData <- getmonitor(i, 'specdata')
-          
-          # sulfateTaken <- subset(curData, sulfate != 'NA')
-          # completeRows <- subset(sulfateTaken, nitrate != 'NA')
-          
-          ## print(completeRows)
-          
-          ## print(curData)
-          ## correld <- cor(completeRows$nitrate, completeRows$sulfate)
-          ## cors <- c(cors, correld)
-        # end
-        
-        # cors
-        # 3
-        ## cor(sufficients$nitrate, sufficients$sulfate)
 }
