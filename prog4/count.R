39db82726cb8396e1e6a8019211bfd85c6894a9c
Alex Auritt <alexauritt@gmail.com>
2014-01-27 22:32:42 -0500

    count.R works, submitted

--- prog4/count.R
+++ prog4/count.R
@@ -6,15 +6,29 @@ count <- function(cause = NULL) {
 ## Return integer containing count of homicides for that cause
   if (!isValidCause(cause))
     stop("invalid cause")
-  6
-}
 
-extractCause <- function(textLine) {
-  "other"
+  homicides <- readLines("homicides.txt")
+  length(grep(getRegex(cause), homicides))
 }
 
 isValidCause <- function(cause) {
   ## checks that specified string is a valid cause. caps are disregarded
   validCauses <- c("asphyxiation", "blunt force", "other", "shooting", "stabbing", "unknown")
   tolower(cause) %in% validCauses
+}
+
+getRegex <-function(cause) {
+  cas <- tolower(cause)
+  if (cas == "asphyxiation")
+    "[Cc]ause: [Aa]sphyxiation"
+  else if (cas == 'blunt force')
+    "[Cc]ause: [Bb]lunt [Ff]orce"
+  else if (cas == 'other')
+    "[Cc]ause: [Oo]ther"
+  else if (cas == 'shooting')
+    "[Cc]ause: [Ss]hooting"
+  else if (cas == 'stabbing')
+    "[Cc]ause: [Ss]tabbing"
+  else if (cas == 'unknown')
+    "[Cc]ause: [Uu]nknown"
 }
